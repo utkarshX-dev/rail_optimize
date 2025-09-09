2b8b9310911813759c063de8c327ba937d3e5805
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-09-09 19:39:21 +0530

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const { activeTrains } = require("./state.js");
 const CompletedJourney = require('./models/completedJourney.js');
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: "*", methods: ["GET","POST","PUT","DELETE"], allowedHeaders: ["Content-Type","Authorization"], credentials: true }));
 app.use(express.json());
